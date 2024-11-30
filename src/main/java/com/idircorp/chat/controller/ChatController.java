cef57af304c61b94a15a9f156970e4cd61368611
Oubeza Idir <oubezaidir@gmail.com>
2024-11-30 21:58:10 +0100

    Add package declarations to WebSocketConfig and ChatController

--- src/main/java/com/idircorp/chat/controller/ChatController.java
+++ src/main/java/com/idircorp/chat/controller/ChatController.java
@@ -1,3 +1,5 @@
+package com.idircorp.chat.controller;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
